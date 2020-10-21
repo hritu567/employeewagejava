5163c63b68396f1647b5f42a448e416fea72fe9e
hritu567 <hritumog96@gmail.com>
2020-10-21 10:43:46 +0530

    Added : calculating Employee Wage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,15 +1,20 @@
-package EmpWageBuilderUC1.java;
+package UC2;
 
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilderUC2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     int IS_FULL_TIME=1;
-     double empCheck=Math.floor(Math.random() * 10) % 2;    
-     if (empCheck == IS_FULL_TIME)
-    	 System.out.println("Employee is Present");
-     else
-    	 System.out.println("Employee is Absent");
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random() * 10) %2;
+		if (empCheck == IS_FULL_TIME)
+			empHrs=8;
+		else
+			empHrs=0;
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+
 	}
 
 }
