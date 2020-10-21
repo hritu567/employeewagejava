14ca7fdc555bd7cf0c0f68dc4c75b7f6d4079498
hritu567 <hritumog96@gmail.com>
2020-10-21 22:45:59 +0530

    Added : max days

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,14 +1,16 @@
-package UC5;
+package UC6;
 
-public class EmpWageBuilderUC5 {
+public class EmpWageBuilderUC6 {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int MAX_HOURS_IN_MONTH=2;
 	public static void main(String[] args) {
-		 int empHrs = 0, empWage=0, totalEmpWage=0;
-		 for (int day=0;day<NUM_OF_WORKING_DAYS;day++)
-		 {
+		int empHrs = 0, totalEmpHrs=0, totalWorkingDays=0;
+		while (totalEmpHrs<=MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+		{
+			 totalWorkingDays++;
 			 int empCheck=(int) Math.floor(Math.random() * 10) %3;
 			 switch (empCheck)
 			 {
@@ -20,13 +22,13 @@ public class EmpWageBuilderUC5 {
 				 break;
 			default:
 				empHrs =0;
-			 }
-			 empWage = empHrs * EMP_RATE_PER_HOUR;
-			 totalEmpWage += empWage;
-			 System.out.println("Emp Wage: " + empWage);
-		 }
-		 System.out.println("Total Emp wage : "+ totalEmpWage);
-
+				
+		}
+			 totalEmpHrs += empHrs;
+			 System.out.println("Day: " + totalWorkingDays + " Emp Hr: " + empHrs);
+    
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Total Emp Wage : " + totalEmpWage);
 	}
-
 }
