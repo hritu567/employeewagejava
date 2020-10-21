63965da4b490b6a5506f38b7be5da1ac91cf0c71
hritu567 <hritumog96@gmail.com>
2020-10-21 10:31:48 +0530

    Added : employee present or not

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,9 +1,15 @@
-package com.wages;
+package EmpWageBuilderUC1.java;
 
-public class EmployeeWages {
+public class EmpWageBuilderUC1 {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage");
+		// TODO Auto-generated method stub
+     int IS_FULL_TIME=1;
+     double empCheck=Math.floor(Math.random() * 10) % 2;    
+     if (empCheck == IS_FULL_TIME)
+    	 System.out.println("Employee is Present");
+     else
+    	 System.out.println("Employee is Absent");
 	}
 
 }
